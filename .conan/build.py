201b689db3b96e0b73b50ab20c29162531bed38e
Bas Stroosnijder <bas@strootje.com>
2017-11-27 12:35:56 +0100

    fixed packageTEsts location in conan build

--- .conan/build.py
+++ .conan/build.py
@@ -1,6 +1,6 @@
 from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-	builder = ConanMultiPackager(args = "-tf ./tests/PackageTest --build missing")
+	builder = ConanMultiPackager(args = "-tf ./tests/PackageTests --build missing")
 	builder.add_common_builds()
 	builder.run()
