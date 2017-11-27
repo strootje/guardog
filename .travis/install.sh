991f4730ebdd3a74a7a2bd3ea09ff12e2688ab96
Bas Stroosnijder <bas@strootje.com>
2017-11-27 12:30:20 +0100

    renamed packagetest folder and fixed path export for travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,7 +21,7 @@ fi
 
 wget https://cmake.org/files/v3.10/cmake-3.10.0-Linux-x86_64.tar.gz -O cmake.tar.gz
 tar -xzf cmake.tar.gz
-export PATH=./cmake-3.10.0-Linux-x86_64/bin;$PATH
+export PATH=./cmake-3.10.0-Linux-x86_64/bin:$PATH
 
 pip install conan --upgrade
 pip install conan_package_tools
