ff6b53cae4d48c95c2091653940026fb1dbd0b91
Bas Stroosnijder <bas@strootje.com>
2017-11-27 15:30:31 +0100

    with pythonpath export maybe

--- .travis/install.sh
+++ .travis/install.sh
@@ -19,6 +19,11 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 	pyenv activate conan
 fi
 
+wget https://cmake.org/files/v3.10/cmake-3.10.0-Linux-x86_64.tar.gz -O cmake.tar.gz
+tar -xzf cmake.tar.gz -C $HOME
+export PATH=$HOME/cmake-3.10.0-Linux-x86_64/bin:$PATH
+export PYTHONPATH=$HOME/cmake-3.10.0-Linux-x86_64/bin:$PYTHONPATH
+
 pip install conan --upgrade
 pip install conan_package_tools
 
